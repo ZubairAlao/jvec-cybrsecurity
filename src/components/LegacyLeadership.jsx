43ca905a00adf20ff884f76b6e0954bacbfa7d26
Alao Zubair Lanre <alaozubair@gmail.com>
2024-11-12 18:21:06 +0100

    assessment page done

--- src/components/LegacyLeadership.jsx
+++ src/components/LegacyLeadership.jsx
@@ -14,7 +14,7 @@ const LegacyLeadership = () => {
                         backgroundRepeat: 'no-repeat',
                     }}
                 ></div>
-                <div className="relative z-20 w-full min-h-[400px] lg:min-h-[550px] flex items-center flex-col justify-center">
+                <div className="relative z-20 w-full min-h-[400px] lg:min-h-[550px] flex items-center flex-col justify-center text-center">
                         <div className="max-w-[860px] space-y-6 p-4">
                             <h1 className='font-bold heading-font-sizes leading-6'>A Legacy of Security Leadership</h1>
                             <p>Our Cyber Executive team brings over 45 years of combined experience across key industries, including Financial Services, Information Technology, and Healthcare, with a global footprint spanning Africa, North America, Europe, and the Middle East.</p>
